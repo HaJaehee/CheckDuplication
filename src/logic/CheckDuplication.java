8004050a81249a33ac4b9ad16852ff579065e1c7
HaJaehee <lovesm135@naver.com>
2017-01-02 19:34:58 +0900

    update
    update

--- src/logic/CheckDuplication.java
+++ src/logic/CheckDuplication.java
@@ -14,26 +14,26 @@ import configuration.Configuration;
 public class CheckDuplication {
 	
 	final File m_folder;
-	private ArrayList<Integer> m_numArr;
+	private ArrayList<String> m_numArr;
 	private ArrayList<String> m_fnameArr;
 	
 	public CheckDuplication () {
 		m_folder = new File (System.getProperty("user.dir"));
-		m_numArr = new ArrayList<Integer>();
+		m_numArr = new ArrayList<String>();
 		m_fnameArr = new ArrayList<String>();
 	}
 	
 	
 	
-	private ArrayList<Integer> getDuplNumArray (ArrayList<Integer> a_numArr) {
+	private ArrayList<String> getDuplNumArray (ArrayList<String> a_numArr) {
 		
 		if ( Configuration.debugging ) {
 			System.out.println("getDuplNumArray");
 		}
 		
-		ArrayList<Integer> duplNumArr = new ArrayList<Integer>();
-		ArrayList<Integer> argNumArr = a_numArr;
-		ArrayList<Integer> numArr = new ArrayList<Integer>();
+		ArrayList<String> duplNumArr = new ArrayList<String>();
+		ArrayList<String> argNumArr = a_numArr;
+		ArrayList<String> numArr = new ArrayList<String>();
 		if (m_numArr.size() == 0) {
 			numArr = getNumArray();
 		} else {
@@ -47,11 +47,17 @@ public class CheckDuplication {
 					System.out.println("check:"+numArr.get(j)+":"+argNumArr.get(i));
 				}
 				if ( numArr.get(j).equals(argNumArr.get(i)) ) {
-					duplNumArr.add(argNumArr.get(i));
-					numArr.remove(j);
-					if ( Configuration.debugging ) {
-						System.out.println("Add duplNumArr:"+argNumArr.get(i));
+					for ( int k = 0 ; k < m_fnameArr.size() ; k++ ) {
+						if ( m_fnameArr.get(k).contains(argNumArr.get(i)) ) {
+							System.out.println(argNumArr.get(i)+" : "+m_fnameArr.get(k));
+							if ( Configuration.debugging ) {
+								System.out.println("Add duplNumArr:"+argNumArr.get(i));
+							}
+						}
+						
 					}
+					duplNumArr.add(argNumArr.get(i));
+					numArr.remove(j--);
 					break;
 				}
 			}
@@ -60,11 +66,11 @@ public class CheckDuplication {
 		return duplNumArr;
 	}
 	
-	private ArrayList<Integer> getNumArray () {
-		ArrayList<Integer> numArr = new ArrayList<Integer>();
+	private ArrayList<String> getNumArray () {
+		ArrayList<String> numArr = new ArrayList<String>();
 		getFilesListForFolder(m_folder);
 		for ( String fileName : m_fnameArr ) {
-			ArrayList<Integer> tempNumArr = extractNumberFromFile(fileName);
+			ArrayList<String> tempNumArr = extractNumberFromFile(fileName);
 			for ( int i = 0 ; i < tempNumArr.size() ; i++ ) {
 				boolean isDupl = false;
 				for ( int j = 0 ; j < numArr.size() ; j++ ) {
@@ -73,7 +79,7 @@ public class CheckDuplication {
 						break;
 					}
 				}
-				if ( !isDupl ) {
+				if ( !isDupl && tempNumArr.get(i).length() >= 3 && !tempNumArr.get(i).equals("1080") ) {
 					numArr.add(tempNumArr.get(i));
 				}
 			}
@@ -88,33 +94,35 @@ public class CheckDuplication {
 				if (fileEntry.isDirectory()) {
 					getFilesListForFolder(fileEntry);
 				} else {
-					m_fnameArr.add(fileEntry.getName());
+					if ( !fileEntry.getName().contains(".dat") ) {
+						m_fnameArr.add(fileEntry.getName());
+					}
 				}
 			}
 		}
 	}
 	
-	private ArrayList<Integer> extractNumberFromFile (String fileName) {
-		ArrayList<Integer> list = new ArrayList<Integer>();
+	private ArrayList<String> extractNumberFromFile (String fileName) {
+		ArrayList<String> list = new ArrayList<String>();
 		String builder = "";
 		for ( int i = 0 ; i < fileName.length() ; i++ ) {
 			if (Character.isDigit(fileName.charAt(i))) {
 				builder += fileName.charAt(i);
 			} else {
 				if (!builder.equals("")) {
-					list.add(Integer.parseInt(builder));
+					list.add(builder);
 					builder = "";
 				}
 			}
 		}
 		if (!builder.equals("")) {
-			list.add(Integer.parseInt(builder));
+			list.add(builder);
 		}
 		return list;
 	}
 	
 	public void saveNumArray (String a_fileNameOfNumArr) {
-		ArrayList<Integer> numArr = new ArrayList<Integer>();
+		ArrayList<String> numArr = new ArrayList<String>();
 		if (m_numArr.size() == 0) {
 			numArr = getNumArray();
 		} else {
@@ -144,7 +152,7 @@ public class CheckDuplication {
 	}
 	
 	public void checkDuplNumArray (String a_fileNameOfNumArr) {
-		ArrayList<Integer> duplNumArr = getDuplNumArray(loadNumArray(a_fileNameOfNumArr));
+		ArrayList<String> duplNumArr = getDuplNumArray(loadNumArray(a_fileNameOfNumArr));
 		
 		String fileNameOfNumArr = System.getProperty("user.dir")+"/dupl_num_arr.txt";
 		File file = new File(fileNameOfNumArr);
@@ -162,13 +170,12 @@ public class CheckDuplication {
 			bw.write(contents);
 			bw.flush();
 			bw.close();
-			System.out.println(contents);
 		} catch (IOException e){
 			e.printStackTrace();
 		} 
 	}
 	
-	private ArrayList<Integer> loadNumArray (String a_fileNameOfNumArr) {
+	private ArrayList<String> loadNumArray (String a_fileNameOfNumArr) {
 		
 		if ( Configuration.debugging ) {
 			System.out.println("loadNumArray");
@@ -177,13 +184,13 @@ public class CheckDuplication {
 		String fileNameOfNumArr = a_fileNameOfNumArr;
 		File file = new File(fileNameOfNumArr);
 		if ( file.exists() ) {
-			ArrayList<Integer> numArr = new ArrayList<Integer>();
+			ArrayList<String> numArr = new ArrayList<String>();
 	
 			try {
 				BufferedReader br = new BufferedReader(new FileReader(file));
 				String line = "";
 				while ( (line = br.readLine()) != null ) {
-					numArr.add(Integer.parseInt(line));
+					numArr.add(line);
 					if ( Configuration.debugging ) {
 						System.out.println(line);
 					}
@@ -196,7 +203,7 @@ public class CheckDuplication {
 			
 			return numArr;
 		} else {
-			return new ArrayList<Integer>();
+			return new ArrayList<String>();
 		}
 	}
 }
